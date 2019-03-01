da8841676751086ca52e2d14674f33bc3fe12cb4
YourSouLi5Mine-Laptop <precichysim@gmail.com>
2019-03-01 15:26:51 -0600

    iteration f1: moving the cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,9 @@
 class StoreController < ApplicationController
   include AccessedTimes
+  include CurrentCart
 
   before_action :set_times, only: [:index]
+  before_action :set_cart
     
   def index
     @products = Product.order(:title)
